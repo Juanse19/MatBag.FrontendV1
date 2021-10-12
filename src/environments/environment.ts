0c8ca29faa23429d7034e8b18653c75fde10ff94
Juan Sebastian Losada <jlosada@mateclogistica.com.co>
2021-10-11 21:20:38 -0500

    Cambios prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,11 +10,11 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://10.100.22.115:3001/api',
+  apiUrl: 'http://10.100.22.89:3002/api',
   apiUrlMat: 'http://10.100.22.48:5001/api',
   apiUrlMatSignalR: 'http://10.100.22.48:5001',
   apiUrlNode: 'http://10.100.22.48:1880',
-  apiUrlNode1: 'http://10.100.22.115:1880',
+  apiUrlNode1: 'http://10.100.22.89:1881',
   // apiUrlNode1: 'http://10.100.22.82:1880',
   apiTeam: 'http://localhost:3000',
   testUser: {
